c385bf16ccbb37359b805f5758f11aca083a6957
andresouzab <andre.desouzabb@gmail.com>
2023-02-23 22:06:12 -0300

    okk

---
+++ js/pagina_loja.js
@@ -0,0 +1,19 @@
+function validarEmail(){
+    const email = "loja01@lojax.com";
+    const password = "x1x2x3"
+
+    if(document.login.email.value.trim() || document.login.password.value.trim() == ""){
+        alert("Você precisa preencher o E-mail e Senha");
+        document.login.email.setfocus();
+        return false;
+    }
+    else if(document.login.email.value.trim() != email && document.login.password.value.trim() != password){  
+        alert("O E-mail está incorreto");
+        document.login.email.setfocus();
+        return false;
+    }
+    else{
+        return true;
+    }
+      
+}
