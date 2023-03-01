e7ff4eebbf1002818cec39e89ff134976af2be84
andresouzab <andre.desouzabb@gmail.com>
2023-02-28 21:32:02 -0300

    ajustes de stylo

--- js/pagina_loja.js
+++ js/pagina_loja.js
@@ -4,12 +4,12 @@ function validarEmail(){
 
     if(document.login.email.value.trim() == "" || document.login.password.value.trim() == ""){
         alert("Você precisa preencher o E-mail e Senha");
-        document.login.email.setfocus();
+        //document.login.email.setfocus();
         return false;
     }
     else if(document.login.email.value.trim() != email || document.login.password.value.trim() != password){  
         alert("O E-mail ou senha está incorreto");
-        document.login.email.setfocus();
+        //document.login.email.setfocus();
         return false;
     }
     else{
